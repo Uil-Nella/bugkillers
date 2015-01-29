7e681d2861ee7f4e6eba0be9fa87aa1da152e254
liuxy <291462491@qq.com>
2015-01-29 15:35:06 +0800

    add full swagger
    Change-Id: Ic5d3f173c9142c2486bfd37267e45d891cea3c3c
    Signed-off-by: liuxy <291462491@qq.com>

--- bugkillers-back/src/main/java/org/bugkillers/back/user/controller/UserController.java
+++ bugkillers-back/src/main/java/org/bugkillers/back/user/controller/UserController.java
@@ -4,8 +4,10 @@ import org.bugkillers.back.bean.User;
 import org.bugkillers.back.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mangofactory.swagger.plugin.EnableSwagger;
@@ -15,7 +17,7 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
-@Api(value="用户API", description="有关于用户的CURD操作", position = 1)
+@Api(value="user-api", description="有关于用户的CURD操作", position = 0)
 @EnableSwagger
 @Controller
 @RequestMapping("/user")
@@ -30,13 +32,21 @@ public class UserController {
 	public void regist( User user){
 		service.save(user);
 	}
-	@ApiOperation(value = "find", position = 2)
+	@ApiOperation(value = "根据pk查找用户",  notes = "Returns a pet when ID < 10. "
+			+ "ID > 10 or nonintegers will simulate API error conditions",response=User.class)
+	@ApiResponses(value = {@ApiResponse(code = 405, message = "Invalid input")})
 	@ResponseBody
-	@RequestMapping(value = { "/find.do" }, method = RequestMethod.GET)
-	public void find(){
-		
+	@RequestMapping(value = { "/{userPk}" }, method = RequestMethod.GET)
+	public User findByPk(@ApiParam(value = "ID of pet that needs to be fetched",  allowableValues = "range[1,5]", required = true
+	,defaultValue = "available",allowMultiple = true) @PathVariable("userPk") @RequestParam("userPk")String userPk){
+		return new User("hahh","哈哈","嘿嘿");
 		
 	}
+	
+	
+	
+	
+	
 	@ApiOperation(value = "hello", position = 3)
 	@ApiResponses(value = {@ApiResponse(code = 405, message = "Invalid input")})
 	@ResponseBody
