14aaa22440f6270bee1b39b91ccb3a1733b99c33
liuxy <291462491@qq.com>
2015-02-06 23:01:01 +0800

    config the root controller.
    Change-Id: I200ac43c547b502a26b16e137bbc9c7ee3038f30
    Signed-off-by: liuxy <291462491@qq.com>

--- bugkillers-back/src/main/java/org/bugkillers/back/user/controller/DispatchController.java
+++ bugkillers-back/src/main/java/org/bugkillers/back/user/controller/DispatchController.java
@@ -1,5 +1,9 @@
 package org.bugkillers.back.user.controller;
 
+import org.apache.ibatis.annotations.ResultMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 跳转分发器
  * <p/>
@@ -8,7 +12,12 @@ package org.bugkillers.back.user.controller;
  * <p> @date 2015年1月30日 下午1:22:33
  * <p> @version 0.0.1
  */
+@Controller
 public class DispatchController {
 	//TODO 
+	@RequestMapping("/job")
+	public String job(){
+		return "index.html";
+	}
 
 }
