fbc029a61db9d030fcfefe9920905964ed7974e2
刘新宇 <liuxinyu03@meituan.com>
2015-06-08 17:42:50 +0800

    添加spring和mvc配置文件

--- bugkillers-core/src/main/java/org/bugkillers/core/domain/TagDO.java
+++ bugkillers-core/src/main/java/org/bugkillers/core/domain/TagDO.java
@@ -11,4 +11,55 @@ public class TagDO {
     private Integer dr;
     private Date bkCreate;
     private Date bkModefied;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public Integer getDr() {
+        return dr;
+    }
+
+    public void setDr(Integer dr) {
+        this.dr = dr;
+    }
+
+    public Date getBkCreate() {
+        return bkCreate;
+    }
+
+    public void setBkCreate(Date bkCreate) {
+        this.bkCreate = bkCreate;
+    }
+
+    public Date getBkModefied() {
+        return bkModefied;
+    }
+
+    public void setBkModefied(Date bkModefied) {
+        this.bkModefied = bkModefied;
+    }
+
+    @Override
+    public String toString() {
+        return "TagDO{" +
+                "id=" + id +
+                ", tagName='" + tagName + '\'' +
+                ", dr=" + dr +
+                ", bkCreate=" + bkCreate +
+                ", bkModefied=" + bkModefied +
+                '}';
+    }
 }
