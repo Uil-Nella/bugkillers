70f992a8813033261bdc398d1a2d64818343d361
刘新宇 <liuxinyu03@meituan.com>
2015-06-26 16:36:36 +0800

    排期

--- bugkillers-core/src/main/java/org/bugkillers/core/config/datesource/DynamicDataSource.java
+++ bugkillers-core/src/main/java/org/bugkillers/core/config/datesource/DynamicDataSource.java
@@ -3,12 +3,7 @@ package org.bugkillers.core.config.datesource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
- * @author yangxuxin
- * @version V1.0
- * @Title: DynamicDataSource.java
- * @Description: 动态数据源类 (一写多读)
- * @Company:meituan
- * @date 15/1/21 19:40
+ *
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
